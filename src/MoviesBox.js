a5f006e9a8eb1978cd9665464860f222bbf8420b
RajatMehta25 <85251518+RajatMehta25@users.noreply.github.com>
2024-10-09 16:48:22 +0530

    Update MoviesBox.js

--- src/MoviesBox.js
+++ src/MoviesBox.js
@@ -53,7 +53,7 @@ const MoviesBox = () => {
           className="iframe"
           src={episode}
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen ; download"
-            allowfullScreen
+            allowfullscreen
         />
       </div>
       <div className="kapilButtonContainer">
