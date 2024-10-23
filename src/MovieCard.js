86330f45fe5877057410498c32f698eea4e327a3
RajatMehta <rajat.mehta@codeaegis.in>
2024-10-23 15:14:18 +0530

    update

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -4,7 +4,7 @@ const MovieCard = ({ photo, link, name }) => {
   return (
     <div className="MovieCardContainer">
       <div style={{ width: "200px", height: "200px", borderRadius: "1rem", padding: "0.5rem" }}>
-        <img src={photo} width="100%" height="100%" />
+        <img src={photo} width="100%" height="100%" style={{ borderRadius: "1rem" }} />
       </div>
       <div>{name}</div>
       <button className="downloadButton">
