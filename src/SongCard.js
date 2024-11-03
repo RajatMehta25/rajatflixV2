615770589f7cc6efb579b085f00450cd33184635
RajatMehta <rajat.mehta@codeaegis.in>
2024-11-03 19:32:48 +0530

    update

--- src/SongCard.js
+++ src/SongCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const SongCard = ({ photo, name, link, setSongPlayLink, setNowPlaying }) => {
   return (
     <div className="MovieCardContainer" style={{ gap: "1rem" }}>
-      <div style={{ width: "100%", height: "200px", borderRadius: "1rem", padding: "0.5rem" }}>
+      <div style={{ width: "200px", height: "200px", borderRadius: "1rem", padding: "0.5rem" }}>
         <img src={photo} width="100%" height="100%" style={{ borderRadius: "1rem" }} />
       </div>
       <div>{name}</div>
