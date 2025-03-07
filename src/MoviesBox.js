b310a2e4b5c5509d244df93587cd203bdc3cd333
RajatMehta <rajat.mehta@codeaegis.in>
2025-03-07 20:11:56 +0530

    update

--- src/MoviesBox.js
+++ src/MoviesBox.js
@@ -240,7 +240,7 @@ const MoviesBox = () => {
             <img
               style={{
                 borderRadius: "1rem",
-                padding: "0.5rem",
+                // padding: "0.5rem",
                 cursor: "pointer",
                 width: "100%",
                 height: "100%",
