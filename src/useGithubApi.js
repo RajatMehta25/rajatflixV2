4d50564e483d05168fc5c7025355c5a708bb2de0
RajatMehta <rajat.mehta@codeaegis.in>
2025-11-18 16:34:05 +0530

    trycatch added in custom hook

--- src/useGithubApi.js
+++ src/useGithubApi.js
@@ -2,13 +2,17 @@ import { useEffect } from "react";
 
 const useGithubApi = (...args) => {
   useEffect(() => {
-    fetch(args[0])
-      .then((res) => res.json())
-      .then((data) => {
-        if (args[1]) args[1](data.data);
-        if (args[2]) args[2](data.data[0].downloadLink ?? "");
-        if (args[3]) args[3](data.data[0].name);
-      });
+    try {
+      fetch(args[0])
+        .then((res) => res.json())
+        .then((data) => {
+          if (args[1]) args[1](data.data);
+          if (args[2]) args[2](data.data[0].downloadLink ?? "");
+          if (args[3]) args[3](data.data[0].name);
+        });
+    } catch (error) {
+      console.error("Error fetching data from GitHub API:", error);
+    }
   }, []);
 };
 
