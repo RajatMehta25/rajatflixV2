0269696d01b0211b71c4c93ab692b110f5d18ac0
RajatMehta <rajat.mehta@codeaegis.in>
2025-11-23 00:19:53 +0530

    search fix

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -2,12 +2,13 @@
 import React, { useContext } from "react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 import { AuthContext } from "./context";
+import LoadingCard from "./LoadingCard";
 
 const ProtectedRoute = ({ fallbackPath = "/login" }) => {
   const { user, authResolved } = useContext(AuthContext);
   const location = useLocation();
 
-  if (!authResolved) return <div>Loading...</div>;
+  if (!authResolved) return <LoadingCard />;
 
   return user ? <Outlet /> : <Navigate to={fallbackPath} replace state={{ from: location }} />;
 };
