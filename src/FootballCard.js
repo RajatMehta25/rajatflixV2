cf3a4b05028457efe602590b030de02b77d19d2f
RajatMehta <rajat.mehta@codeaegis.in>
2025-11-25 21:16:10 +0530

    css update

--- src/FootballCard.js
+++ src/FootballCard.js
@@ -1,49 +1,31 @@
 import moment from "moment";
 import React from "react";
+import "./FootballCard.css";
 
 const FootballCard = ({ homeLogo, awayLogo, homeName, awayName, status, time, score, league, onClick }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flex: 1,
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        gap: "0.5rem",
-        padding: "1rem",
-        border: "1px solid #373b44",
-        borderRadius: "1rem",
-        // pointerEvents: "none",
-        textWrap: "nowrap",
-        cursor: "pointer",
-      }}
-      onClick={onClick}
-    >
-      <div>{league}</div>
-      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "0.5rem" }}>
-        <div
-          style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "0.5rem" }}
-        >
-          <span>{homeName}</span>
+    <div className="football-card" onClick={onClick}>
+      <div className="league">{league}</div>
+      <div className="teams">
+        <div className="team">
+          <span className="team-name">{homeName}</span>
           <img
-            style={{ width: "50px", height: "50px", borderRadius: "50%" }}
-            src={`https://streamed.pk/api/images/proxy/${homeLogo}.webp`}
+            className="team-logo"
+            src={
+              `https://streamed.pk/api/images/proxy/${homeLogo}.webp` ||
+              "https://github.com/RajatMehta25/rajatflixV2/blob/main/public/rajatflix.png?raw=true"
+            }
+            alt={`${homeName} logo`}
           />
         </div>
         <span>VS</span>
-        <div
-          style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "0.5rem" }}
-        >
-          <span>{awayName}</span>
-          <img
-            style={{ width: "50px", height: "50px", borderRadius: "50%" }}
-            src={`https://streamed.pk/api/images/proxy/${awayLogo}.webp`}
-          />
+        <div className="team">
+          <span className="team-name">{awayName}</span>
+          <img className="team-logo" src={`https://streamed.pk/api/images/proxy/${awayLogo}.webp`} alt={`${awayName} logo`} />
         </div>
       </div>
 
-      {status === 1 ? <span style={{ color: "red" }}>Live</span> : <span>{moment(time).format("MMMM Do YYYY, h:mm a")}</span>}
+      {status === 1 ? <span className="status-live">Live</span> : <span>{moment(time).format("MMMM Do YYYY, h:mm a")}</span>}
       {status === 1 ? <div>{score}</div> : false}
     </div>
   );
